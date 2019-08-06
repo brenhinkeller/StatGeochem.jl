ac4edd258ad1a6854af56819aae70f46958e393d
C. Brenhin Keller <cbkeller@berkeley.edu>
2019-08-06 02:40:58 -0400

    New sequential colormap

--- src/utilities/Colormaps.jl
+++ src/utilities/Colormaps.jl
@@ -38,6 +38,9 @@
         )
     export water
 
+    lines = parse.(Colorant, ["#0072BD","#D95319","#EDB120","#7E2F8E","#77AC30","#4DBEEE","#A2142F",])
+    export lines
+
 ## --- Resize and interpolate colormaps
 
     # Linearly interpolate cmap at positions xq
