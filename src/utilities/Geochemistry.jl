1f0aafda5da4e36059f99aa28966a24a4559c287
C. Brenhin Keller <cbkeller@berkeley.edu>
2019-12-04 19:45:10 -0500

    Can specify unit conversions in oxideconversion. Expects metals as PPM, oxides as wt.% by default

--- src/utilities/Geochemistry.jl
+++ src/utilities/Geochemistry.jl
@@ -103,7 +103,16 @@
 
 ## --- Oxide conversions
 
-    function oxideconversion(dataset::Dict)
+    """
+    ```julia
+    dataset = oxideconversion(dataset::Dict; unitratio::Number=10000)
+    ```
+
+    Convert major elements (Ti, Al, etc.) into corresponding oxides (TiO2, Al2O3)...
+    If metals are as PPM, set unitratio=10000 (default); if metals are as wt%,
+    set unitratio = 1
+    """
+    function oxideconversion(dataset::Dict; unitratio::Number=10000)
         # Convert major elements (Ti, Al, etc.) into corresponding oxides (TiO2, Al2O3)...
         # for i=1:length(source)
         #     conversionfactor(i)=mass.percation.(dest[i])./mass.(source[i]);
@@ -112,7 +121,7 @@
         # Array of elements to convert
         source = ["Si","Ti","Al","Fe","Fe","Mg","Ca","Mn","Na","K","P","Cr","Ni","Co","C","S","H"]
         dest = ["SiO2","TiO2","Al2O3","FeOT","Fe2O3T","MgO","CaO","MnO","Na2O","K2O","P2O5","Cr2O3","NiO","CoO","CO2","SO2","H2O"]
-        conversionfactor=[2.13932704290547,1.66847584248889,1.88944149488507,1.28648836426407,1.42973254639611,1.65825961736268,1.39919258253823,1.29121895771597,1.34795912485574,1.20459963614796,2.29133490474735,1.46154369861159,1.27258582901258,1.27147688434143,3.66405794688203,1.99806612601372,8.93601190476191]
+        conversionfactor = [2.13932704290547,1.66847584248889,1.88944149488507,1.28648836426407,1.42973254639611,1.65825961736268,1.39919258253823,1.29121895771597,1.34795912485574,1.20459963614796,2.29133490474735,1.46154369861159,1.27258582901258,1.27147688434143,3.66405794688203,1.99806612601372,8.93601190476191]
 
         # If source field exists, fill in destination from source
         for i = 1:length(source)
@@ -121,7 +130,7 @@
                     dataset[dest[i]] = fill(NaN, size(dataset[source[i]]))
                 end
                 t = isnan.(dataset[dest[i]]) .& (.~ isnan.(dataset[source[i]]))
-                dataset[dest[i]][t] = dataset[source[i]][t] .* conversionfactor[i]
+                dataset[dest[i]][t] = dataset[source[i]][t] .* (conversionfactor[i] / unitratio)
             end
         end
 
