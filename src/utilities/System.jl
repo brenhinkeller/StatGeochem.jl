3bf410e1518af8c220f90a340cc443ffe2a54ff9
C. Brenhin Keller <cbkeller@berkeley.edu>
2020-07-01 16:22:57 -0400

    Better conditional exporting of 'contains', etc.

--- src/utilities/System.jl
+++ src/utilities/System.jl
@@ -18,7 +18,7 @@
 ## --- Retain deprecated functions with matlab-like syntax,
     # to avoid breakages in user scripts that may depend on them
 
-    if VERSION>=v"1.0"
+    if ~ @isdefined linspace
         """
         ```julia
         linspace(l::Number,u::Number,n::Number)
@@ -31,7 +31,9 @@
             return range(l,stop=u,length=n)
         end
         export linspace
+    end
 
+    if ~ @isdefined linreg
         """
         ```julia
         (a,b) = linreg(x::AbstractVector, y::AbstractVector)
@@ -42,54 +44,33 @@
         """
         linreg(x::AbstractVector, y::AbstractVector) = hcat(fill!(similar(x), 1), x) \ y
         export linreg
+    end
 
-        """
-        ```julia
-        contains(haystack::AbstractString, needle::Union{AbstractString,Regex,AbstractChar})
-        ```
-
-        Tests whether the string `haystack` contains the string or char `needle`,
-        matching case. Identical to `occursin`, but with opposite argument order.
-        """
-        function contains(haystack::AbstractString, needle::Union{AbstractString,Regex,AbstractChar})
-            return occursin(needle, haystack)
-        end
+    if ~ @isdefined contains
         """
         ```julia
         contains(haystack, needle)
         ```
 
-        Converts both `haystack` and `needle` to strings and checks whether
-        `string(haystack)` contains `string(needle)`.
+        Converts both `haystack` and `needle` to strings (if not already strings)
+        and checks whether `string(haystack)` contains `string(needle)`.
         """
-        function contains(haystack, needle)
-            return occursin(string(needle), string(haystack))
-        end
+        contains(haystack::AbstractString, needle::Union{AbstractString,Regex,AbstractChar}) = occursin(needle, haystack)
+        contains(haystack, needle) = occursin(string(needle), string(haystack))
         export contains
-
-        """
-        ```julia
-        containsi(haystack::AbstractString, needle::Union{AbstractString,AbstractChar})
-        ```
-
-        Tests whether the string `haystack` contains the string or char `needle`,
-        ignoring case
-        """
-        function containsi(haystack::AbstractString, needle::Union{AbstractString,AbstractChar})
-            return occursin(lowercase(needle), lowercase(haystack))
-        end
-        """
-        ```julia
-        containsi(haystack, needle)
-        ```
-
-        Converts both `haystack` and `needle` to strings and checks whether
-        `string(haystack)` contains `string(needle)`, ignoring case.
-        """
-        function containsi(haystack, needle)
-            return occursin(lowercase(string(needle)), lowercase(string(haystack)))
-        end
-        export containsi
     end
+
+    """
+    ```julia
+    containsi(haystack, needle)
+    ```
+
+    Converts both `haystack` and `needle` to strings and checks whether
+    `string(haystack)` contains `string(needle)`, ignoring case.
+    """
+    containsi(haystack::AbstractString, needle::Union{AbstractString,AbstractChar}) = occursin(lowercase(needle), lowercase(haystack))
+    containsi(haystack, needle) = occursin(lowercase(string(needle)), lowercase(string(haystack)))
+    export containsi
+
 
 ## --- End of File
