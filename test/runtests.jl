158279a27122c28355c790ad122da8d4d89cc8e1
C. Brenhin Keller <cbkeller@dartmouth.edu>
2020-10-03 19:30:00 -0400

    Try using testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 using StatGeochem
 using Test, Statistics, StatsBase
 
-include("testUtilities.jl")
-include("testArrayStats.jl")
-include("testImport.jl")
-include("testResources.jl")
-include("testCrust1.jl")
+@testset "ArrayStats" begin include("testArrayStats.jl") end
+@testset "Import" begin include("testImport.jl") end
+@testset "Other Utilities" begin include("testUtilities.jl") end
+@testset "Crust 1.0" begin include("testCrust1.jl") end
+@testset "Other Resources" begin include("testResources.jl") end
