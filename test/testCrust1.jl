69160675d901bf3ea6a5fef1ea2fdc06485fd91a
C. Brenhin Keller <cbkeller@dartmouth.edu>
2021-01-21 16:01:54 -0500

    Update tests for crust 1.0

--- test/testCrust1.jl
+++ test/testCrust1.jl
@@ -1,5 +1,8 @@
 ## -- Test getting seismic data
 
+	# Have to download crust 1 before testing it!
+	@test get_crust1() == 0
+
 	# Test single lat-lon-depth point from crust1
 	@test [x[1] for x in find_crust1_seismic(50,50,8)] == [7.1, 4.05, 3000.0]
 
