c630202454bf1069d2a42b3a221db88e68bf78da
C.Brenhin Keller <cbkeller@dartmouth.edu>
2021-01-24 21:52:36 -0500

    Test both fixed and variable changepoint number methods

--- test/testChangepoint.jl
+++ test/testChangepoint.jl
@@ -12,7 +12,7 @@
     dist = changepoint(A, ones(200), 10000; np=1)
     @test isapprox(nanmean(dist[burnin:end]), 101, atol=2)
     dist = changepoint(A, ones(200), 10000; npmin=1, npmax=5)[burnin:end,:]
-    @test isapprox(nanmean(dist[dist.>0])), 101, atol=2)
+    @test isapprox(nanmean(dist[dist.>0]), 101, atol=2)
 
 
 ## --
