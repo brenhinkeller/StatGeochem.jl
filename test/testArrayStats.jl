f68e01ba521c60498a5f4ce8e793ab838d6d1a82
C.Brenhin Keller <cbkeller@dartmouth.edu>
2021-01-24 20:22:02 -0500

    Tests for weighted nanstd

--- test/testArrayStats.jl
+++ test/testArrayStats.jl
@@ -1,85 +1,95 @@
 ## --- ArrayStats.jl
 
+    # Filtering
+    A = 1:100
+    @test A[inpctile(A,80)] == 11:90
+
+    # NaN handling functions, simple cases
+    A = [1:10; fill(NaN,10)]
+    B = [fill(NaN,10); 11:20]
+    @test A[nanmask(A)] == 1:10
+    @test nanadd(A,B) == 1:20
+    @test nanadd!(A,B) == 1:20
+    @test zeronan!(B) == [fill(0,10); 11:20]
+
     # Summary statistics: simple cases, Float64
     A = [1:10.0..., NaN]
-    @test StatGeochem.nanmin(1.,2.) == 1.
-    @test StatGeochem.nanmax(1.,2.) == 2.
     @test nansum(A) == 55.0
     @test nanmean(A) == 5.5
+    @test nanmean(A, ones(11)) == 5.5 # weighted
     @test nanrange(A) == 9.0
     @test nanminimum(A) == 1.0
     @test nanmaximum(A) == 10.0
     @test nanextrema(A) == (1.0, 10.0)
     @test nanstd([1,2,3,NaN]) == 1.0
+    @test nanstd([1,2,3,NaN], ones(4)) == 1.0 # weighted
     @test nanmad([1,2,3,NaN]) == 1.0
     @test nanaad([1,2,3,NaN]) ≈ 2/3
     @test nanmedian([1,2,3,NaN]) == 2.0
     @test pctile([0:100...,NaN],99) == 99.0
-
-    # NaN handling, simple cases
-    A = [1:10; fill(NaN,10)]
-    B = [fill(NaN,10); 11:20]
-    @test nanadd(A,B) == 1:20
-    @test nanadd!(A,B) == 1:20
-    @test zeronan!(B) == [fill(0,10); 11:20]
+    @test StatGeochem.nanmin(1.,2.) == 1.
+    @test StatGeochem.nanmax(1.,2.) == 2.
 
     # Arrays containing only NaNs should yield NaN
     A = fill(NaN,10)
     @test nansum(A) == 0
     @test isnan(nanmean(A))
-    @test isnan(nanmean(A, ones(10)))
+    @test isnan(nanmean(A, ones(10))) # weighted
     @test isnan(nanrange(A))
     @test isnan(nanminimum(A))
     @test isnan(nanmaximum(A))
     @test all(isnan.(nanextrema(A)))
     @test isnan(nanstd(A))
+    @test isnan(nanstd(A, ones(10))) # weighted
     @test isnan(nanaad(A))
+    @test isnan(StatGeochem.nanmin(NaN,NaN))
+    @test isnan(StatGeochem.nanmax(NaN,NaN))
 
     # Summary statistics: simple cases, Int64
     A = collect(1:10)
-    @test StatGeochem.nanmin(1,2) == 1
-    @test StatGeochem.nanmax(1,2) == 2
     @test nansum(A) == 55.0
     @test nanmean(A) == 5.5
-    @test nanmean(A, ones(10)) == 5.5
+    @test nanmean(A, ones(10)) == 5.5 # weighted
     @test nanrange(A) == 9.0
     @test nanminimum(A) == 1.0
     @test nanmaximum(A) == 10.0
     @test nanextrema(A) == (1.0, 10.0)
     @test nanstd([1,2,3]) == 1.0
+    @test nanstd([1,2,3], ones(3)) == 1.0 # weighted
     @test nanmad([1,2,3]) == 1.0
     @test nanaad([1,2,3]) ≈ 2/3
     @test nanmedian([1,2,3]) == 2.0
     @test pctile([0:100...],99) == 99.0
+    @test StatGeochem.nanmin(1,2) == 1
+    @test StatGeochem.nanmax(1,2) == 2
 
     # Summary statistics: simple cases, ranges
     A = 1:10
-    @test StatGeochem.nanmin(1,2) == 1
-    @test StatGeochem.nanmax(1,2) == 2
     @test nansum(A) == 55.0
     @test nanmean(A) == 5.5
-    @test nanmean(A, ones(10)) == 5.5
+    @test nanmean(A, ones(10)) == 5.5 # weighted
     @test nanrange(A) == 9.0
     @test nanminimum(A) == 1.0
     @test nanmaximum(A) == 10.0
     @test nanextrema(A) == (1.0, 10.0)
     @test nanstd(1:3) == 1.0
+    @test nanstd(1:3, ones(3)) == 1.0 # weighted
     @test nanmad(1:3) == 1.0
     @test nanaad(1:3) ≈ 2/3
     @test nanmedian(1:3) == 2.0
     @test pctile(0:100,99) == 99.0
 
+
     A = 1:10.
-    @test StatGeochem.nanmin(1,2) == 1
-    @test StatGeochem.nanmax(1,2) == 2
     @test nansum(A) == 55.0
     @test nanmean(A) == 5.5
-    @test nanmean(A, ones(10)) == 5.5
+    @test nanmean(A, ones(10)) == 5.5 # weighted
     @test nanrange(A) == 9.0
     @test nanminimum(A) == 1.0
     @test nanmaximum(A) == 10.0
     @test nanextrema(A) == (1.0, 10.0)
     @test nanstd(1:3.) == 1.0
+    @test nanstd(1:3., ones(3)) == 1.0 # weighted
     @test nanmad(1:3.) == 1.0
     @test nanaad(1:3.) ≈ 2/3
     @test nanmedian(1:3.) == 2.0
@@ -93,10 +103,12 @@
     @test nanmaximum(A, dims=2) == maximum(A, dims=2)
     @test nanmean(A, dims=1) == mean(A, dims=1)
     @test nanmean(A, dims=2) == mean(A, dims=2)
-    @test nanmean(A, ones(size(A)), dims=1) == mean(A, dims=1)
-    @test nanmean(A, ones(size(A)), dims=2) == mean(A, dims=2)
+    @test nanmean(A, ones(size(A)), dims=1) == mean(A, dims=1) # weighted
+    @test nanmean(A, ones(size(A)), dims=2) == mean(A, dims=2) # weighted
     @test nanstd(A, dims=1) ≈ std(A, dims=1)
     @test nanstd(A, dims=2) ≈ std(A, dims=2)
+    @test nanstd(A, ones(size(A)), dims=1) ≈ std(A, dims=1) # weighted
+    @test nanstd(A, ones(size(A)), dims=2) ≈ std(A, dims=2) # weighted
     @test nanmad(A, dims=1) == [25.0 25.0 25.0]
     @test nanmad(A, dims=2) == fill(100.0,100,1)
     @test nanaad(A, dims=1) == [25.0 25.0 25.0]
@@ -115,10 +127,12 @@
     @test nanmaximum(A, dims=2) == maximum(A, dims=2)
     @test nanmean(A, dims=1) == mean(A, dims=1)
     @test nanmean(A, dims=2) == mean(A, dims=2)
-    @test nanmean(A, ones(size(A)), dims=1) == mean(A, dims=1)
-    @test nanmean(A, ones(size(A)), dims=2) == mean(A, dims=2)
+    @test nanmean(A, ones(size(A)), dims=1) == mean(A, dims=1) # weighted
+    @test nanmean(A, ones(size(A)), dims=2) == mean(A, dims=2) # weighted
     @test nanstd(A, dims=1) ≈ std(A, dims=1)
     @test nanstd(A, dims=2) ≈ std(A, dims=2)
+    @test nanstd(A, ones(size(A)), dims=1) ≈ std(A, dims=1) # weighted
+    @test nanstd(A, ones(size(A)), dims=2) ≈ std(A, dims=2) # weighted
     @test nanmad(A, dims=1) == [25.0 25.0 25.0]
     @test nanmad(A, dims=2) == fill(100.0,100,1)
     @test nanaad(A, dims=1) == [25.0 25.0 25.0]
