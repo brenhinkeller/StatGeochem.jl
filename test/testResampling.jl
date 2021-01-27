7db497b97875f757b6d7197aea31eda381ebe470
C.Brenhin Keller <cbkeller@dartmouth.edu>
2021-01-27 00:41:12 -0500

    Adapt tests to new bsr format

--- test/testResampling.jl
+++ test/testResampling.jl
@@ -1,9 +1,21 @@
 ## --- Resampling.jl
 
-    @test bsr!(Array{Int64}(undef,1000),Array{Int64}(undef,1000),1:10,0,1000,0.5) |> unique ⊆ 1:10
-    @test bsr!(Array{Int64}(undef,1000),Array{Int64}(undef,1000),1:10,0,1000,fill(0.5,1000)) |> unique ⊆ 1:10
-    @test bsr!(Array{Int64}(undef,1000),Array{Int64}(undef,1000),1:10,fill(0,10),1000,0.5) |> unique ⊆ 1:10
-    @test bsr!(Array{Int64}(undef,1000),Array{Int64}(undef,1000),1:10,fill(0,10),1000,fill(0.5,1000)) |> unique ⊆ 1:10
+    index = Array{Int64}(undef,1000)
+    resampled = Array{Int64}(undef,1000)
+    @test bsr!(resampled,index,1:10,0,0.5) |> unique ⊆ 1:10
+    @test bsr!(resampled,index,1:10,0,fill(0.5,1000)) |> unique ⊆ 1:10
+    @test bsr!(resampled,index,1:10,fill(0,10),0.5) |> unique ⊆ 1:10
+    @test bsr!(resampled,index,1:10,fill(0,10),fill(0.5,1000)) |> unique ⊆ 1:10
+
+    resampled = Array{Int64}(undef,1000)
+    bsr!(resampled, index, ones(10), 0.5, 1000, 0.5)
+    @test isapprox(mean(reasampled), 1, atol=0.15)
+    bsr!(resampled, index, ones(10), 0.5, 1000, fill(0.5,1000))
+    @test isapprox(mean(reasampled), 1, atol=0.15)
+    bsr!(resampled, index, ones(10), fill(0.5,10), 1000, 0.5)
+    @test isapprox(mean(reasampled), 1, atol=0.15)
+    bsr!(resampled, index, ones(10), fill(0.5,10), 1000, fill(0.5,1000))
+    @test isapprox(mean(reasampled), 1, atol=0.15)
 
     @test bsresample(1:10,fill(0.5,10),1000,0.5)::Array{Float64} |> length == 1000
     @test bsresample(1:10,fill(0.5,10),1000,fill(0.5,10))::Array{Float64} |> length == 1000
