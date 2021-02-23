63a85e45d23abec350beadd22e6aeb57ec402ecb
C.Brenhin Keller <cbkeller@dartmouth.edu>
2021-02-22 23:44:00 -0500

    Back-port improved nanmedian! binning from nanstatistics

--- test/testArrayStats.jl
+++ test/testArrayStats.jl
@@ -161,21 +161,24 @@
     @test pctile(A, 10, dim=2) ≈ 21:120
 
     # Summary statistics: binning
-    # Means
+    M3 = zeros(3)
+    N3 = fill(0,3)
+    M33 = zeros(3,3)
+    N33 = fill(0,3,3)
+    r = [17.0 117.0 217.0; 50.0 150.0 250.0; 83.0 183.0 283.0]
+    # 1. Means
     @test nanmean([1:100..., 1], [1:100..., NaN], 0, 100, 3) == [17, 50, 83]
     nanmean!(M3, N3, [1:100..., 1],[1:100..., NaN],0,100,3)
     @test M3 == [17, 50, 83]
     @test N3 == fill(33,3)
     @test nanmean(1:100, reshape(1:300,100,3), 0, 100, 3) == r
-
-    # Weighted means
+    # 2. Weighted means
     @test nanmean([1:100..., 1], [1:100..., NaN], ones(100), 0,100,3) == [17, 50, 83]
     @test nanmean(1:100, reshape(1:300,100,3), ones(100), 0, 100, 3) == r
     nanmean!(M33, N33, 1:100, reshape(1:300,100,3), 0, 100, 3)
     @test M33 == r
     @test N33 == fill(33,3,3)
-
-    # Medians
+    # 3. Medians
     @test nanmedian([1:100..., 1],[1:100..., NaN],0,100,3) == [17, 50, 83]
     nanmedian!(M3, N3, [1:100..., 1],[1:100..., NaN],0,100,3)
     @test M3 == [17, 50, 83]
