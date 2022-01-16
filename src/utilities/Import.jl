93740429d1f2b48fc64db5ad423c8880f2b7b067
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-01-15 20:39:15 -0500

    Add example to unelementify docstring

--- src/utilities/Import.jl
+++ src/utilities/Import.jl
@@ -571,7 +571,22 @@
         \trows=:
     )
     ```
-    Convert a dict or named tuple of vectors into a 2-D array with variables as columns
+    Convert a Dict or Named Tuple of vectors into a 2-D array with variables as columns
+
+    ### Examples
+    ```julia
+    julia> D
+    NamedTuple with 3 elements:
+      La  = Vector{Float64}(2,) [1.5 ... 3.7]
+      Ce  = Vector{Float64}(2,) [1.1 ... 2.9]
+      Pr  = Vector{Float64}(2,) [1.0 ... 2.5]
+
+    julia> unelementify(D)
+    3×3 Matrix{Any}:
+      "La"   "Ce"   "Pr"
+     1.5    1.1    1.0
+     3.7    2.9    2.5
+    ```
     """
     function unelementify(dataset::Dict, elements::Array=sort(collect(keys(dataset)));
             floatout::Bool=false,
