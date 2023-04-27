6fcc29495a1a8b0d58f924641ab358a2be556577
C. Brenhin Keller <cbkeller@dartmouth.edu>
2023-04-27 13:27:42 -0400

    Clean up find_seafloorage

--- src/resources/Seafloorage.jl
+++ src/resources/Seafloorage.jl
@@ -103,29 +103,25 @@
     find_seafloorage(lat, lon) = find_seafloorage(get_seafloorage("seafloorage"), lat, lon)
     find_seafloorage(sfdata::Dict, lat, lon) = find_seafloorage(sfdata["seafloorage"], lat, lon)
     function find_seafloorage(sfdata::AbstractArray, lat, lon)
-
         # Interpret user input
-        length(lat) != length(lon) && error("`lat` and `lon` must be of equal length\n")
-
-        # Find the column numbers (using mod to convert lon from -180:180 to 0:360
-        x = floor.(Int, mod.(lon, 360) * 10800/360) .+ 1
-
-        # find the y rows, converting from lat to Mercator (lat -80.738:80.738)
-        y = 4320 .- floor.(Int, 8640 * asinh.(tan.(lat*pi/180)) / asinh.(tan.(80.738*pi/180)) / 2 ) .+ 1
+        eachindex(lat) == eachindex(lon) || error("`lat` and `lon` must be the same size")
 
         # Make and fill output array
-        out=Array{Float64}(undef,size(x))
-        for i ∈ eachindex(x)
+        result = fill(NaN, size(lat))
+        for i ∈ eachindex(lat)
+            # Find the column numbers (using mod to convert lon from -180:180 to 0:360
+            x = floor(Int, mod(lon[i], 360) * 10800/360) + 1
+
+            # find the y rows, converting from lat to Mercator (lat -80.738:80.738)
+            y = 4320 - floor(Int, 8640 * asinh(tan(lat[i]*pi/180)) / asinh(tan(80.738*pi/180)) / 2 ) + 1
+
             # If there is out data for row(i), col(i)
-            if isnan(x[i]) || isnan(y[i]) || x[i]<1 || x[i]>10800 || y[i]<1 || y[i]>8640
-                out[i] = NaN
-            else
+            if (1 <= x <= 10800) && (1 <= y <= 8640)
                 # Then fill in the output data (Age, Age_Min, Age_Max)
-                out[i] = sfdata[y[i], x[i]]
+                result[i] = sfdata[y, x]
             end
         end
-
-        return out
+        return result
     end
     export find_seafloorage
 
