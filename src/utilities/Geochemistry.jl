f73380071a83df60b457dbaae06f50a2affd3605
Jannitta Yao <jyao@wellesley.edu>
2023-11-28 12:50:44 -0500

    Update Geochemistry.jl

--- src/utilities/Geochemistry.jl
+++ src/utilities/Geochemistry.jl
@@ -824,7 +824,7 @@
 
         # Turn on nonlinear subdivision and change resolution
         if nonlinear_subdivision
-            system("sed -e \"s/non_linear_switch .*|/nonlinear_subdivision              T |/\" -i.backup $(prefix)perplex_option.dat")
+            system("sed -e \"s/non_linear_switch .*|/non_linear_switch              T |/\" -i.backup $(prefix)perplex_option.dat")
             system("sed -e \"s/initial_resolution .*|/initial_resolution              1/2 1/4 |/\" -i.backup $(prefix)perplex_option.dat")
         end
 
