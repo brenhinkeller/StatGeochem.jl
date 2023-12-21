d52956125c9a6cc046b1d30297ba64f0c05dc8d2
RowanGregoire <rowan.k.gregoire.23@dartmouth.edu>
2023-12-21 17:24:55 -0500

    Export `spidergram` functions

--- src/StatGeochem.jl
+++ src/StatGeochem.jl
@@ -48,6 +48,8 @@ module StatGeochem
     # Methods exclusively for package extensions
     mapplot() = @warn "`mapplot` requires Plots.jl"
     mapplot!() = @warn "`mapplot!` requires Plots.jl"
-    export mapplot, mapplot!
+    mapplot() = @warn "`spidergram` requires Plots.jl"
+    mapplot!() = @warn "`spidergram!` requires Plots.jl"
+    export mapplot, mapplot!, spidergram, spidergram!
 
 end # module
